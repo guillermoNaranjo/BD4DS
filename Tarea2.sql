c32a70d3066d364ae3a41bf1713d8543d21df250
guillermoNaranjo <70484786+guillermoNaranjo@users.noreply.github.com>
2021-03-15 17:46:03 -0600

    Update Tarea2.sql

--- Tarea2.sql
+++ Tarea2.sql
@@ -9,8 +9,8 @@ select concat(c2.first_name, ' ', c2.last_name), c2.email from country c join ci
 address a using (city_id) join store s using (address_id) join customer c2 using (store_id) where c.country='Canada'; 
 --Cuál es nuestro revenue por store? (suma del amount agrupado por tienda)
 select store_id,  sum(p.amount) from payment p join staff s using (staff_id) join store using (store_id) group by store_id; 
---Qué películas son las más rentadas en todas nuestras stores? (top 10 más rentadas en todas las tiendas)
-select count(i.film_id) as "Numero de rentas", f.title as "Pelicula" from rental r 
+--Qué películas son las más rentadas en todas nuestras stores? (La más rentada de cada tienda)
+select count(i.film_id) as "Numero de rentas", f.title as "Pelicula", s.store_id from rental r 
 join inventory i using (inventory_id) join store s using 
 (store_id) join film f using (film_id) 
-group by f.title order by count(i.film_id) desc limit 10;
+group by f.title, s.store_id order by count(i.film_id) desc limit 2;
