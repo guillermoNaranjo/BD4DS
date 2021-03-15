5d39a7f521e08384701a0d0c6de92fb7c3e4bf2d
guillermoNaranjo <70484786+guillermoNaranjo@users.noreply.github.com>
2021-03-15 13:58:20 -0600

    Update Tarea2.sql

--- Tarea2.sql
+++ Tarea2.sql
@@ -1,15 +1,15 @@
---QuÈ pelÌculas son las m·s rentadas en todas nuestras stores?
+--Qu√© cliente renta m√°s pel√≠culas para adultos? fueron 2 con el mismo n√∫mero
 select c.customer_id, concat(c.first_name,' ',c.last_name) as "Name", count(f.film_id) from film f 
 join inventory i using (film_id) join rental r using 
 (inventory_id) join customer c using (customer_id) where 
 f.rating ='NC-17' group by c.customer_id, "Name" order by count(f.film_id)
 desc limit 2;
---CÛmo obtenemos todos los nombres y correos de nuestros clientes canadienses para una campaÒa?
+--C√≥mo obtenemos todos los nombres y correos de nuestros clientes canadienses para una campa√±a?
 select concat(c2.first_name, ' ', c2.last_name), c2.email from country c join city using (country_id) join 
 address a using (city_id) join store s using (address_id) join customer c2 using (store_id) where c.country='Canada'; 
---Cu·l es nuestro revenue por store?
+--Cu√°l es nuestro revenue por store? (suma del amount agrupado por tienda)
 select store_id,  sum(p.amount) from payment p join staff s using (staff_id) join store using (store_id) group by store_id; 
---QuÈ pelÌculas son las m·s rentadas en todas nuestras stores? (top 10 m·s rentadas en todas las tiendas)
+--Qu√© pel√≠culas son las m√°s rentadas en todas nuestras stores? (top 10 m√°s rentadas en todas las tiendas)
 select count(i.film_id) as "Numero de rentas", f.title as "Pelicula" from rental r 
 join inventory i using (inventory_id) join store s using 
 (store_id) join film f using (film_id) 
