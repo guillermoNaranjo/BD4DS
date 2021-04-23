e24d8faf40b7d5bfd64ada1bd0c5c2613748b6cf
guillermoNaranjo <70484786+guillermoNaranjo@users.noreply.github.com>
2021-04-23 11:56:19 -0500

    Update tarea5.sql

--- tarea5.sql
+++ tarea5.sql
@@ -2,9 +2,18 @@ with pagos as (
 	select o.customer_id as id_c ,sum(od.quantity*od.unit_price) as total, o.order_id, o.order_date as fecha from order_details od join orders o using (order_id) group by o.order_id, o.customer_id order by o.order_id, fecha 
 ), pagos_cliente as (
 	select p.total as tot, p.id_c as id, extract('month' from p.fecha) as mes, p.fecha as f from pagos p order by p.id_c
-) select pc.id, pc.tot as precio_a, lead(pc.tot) over w as precio_Amas1, 
-((lead(pc.tot) over w - pc.tot)/pc.tot)*100 as porcentaje from pagos_cliente pc window w as 
-(partition by pc.id order by pc.f);
+), delta as ( 
+	select pc.id as cliente, pc.tot as precio_a, lag(pc.tot) over w as precio_Amas1, 
+	(pc.tot-lag(pc.tot) over w) as diferencia from pagos_cliente pc window w as 
+	(partition by pc.id order by pc.f)
+), diferencias as ( 
+	select d.cliente as cliente, sum(d.diferencia)/(count(*)-1) as crecimiento from delta d group by d.cliente
+) select d.cliente, d.crecimiento, case 
+	when d.crecimiento<=0 then 'Malo'
+	when d.crecimiento>=0 and d.crecimiento<=100 then 'regular'
+	when d.crecimiento>100 then 'Bueno' 
+	else 'Bueno'
+	end as "Categoría cliente" from diferencias d 
 
 
 
